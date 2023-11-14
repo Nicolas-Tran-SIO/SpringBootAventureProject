24d436441c773ec5c6e1e98f9c0ffe8981d75a4a
TRAN Nicolas <nicolas7725@gmail.com>
2023-11-14 11:15:06 +0100

    First commit

---
+++ src/main/kotlin/sio/springbootaventureproject/SpringBootAventureProjectApplication.kt
@@ -0,0 +1,11 @@
+package sio.springbootaventureproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootAventureProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootAventureProjectApplication>(*args)
+}
---
+++ src/test/kotlin/sio/springbootaventureproject/SpringBootAventureProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package sio.springbootaventureproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootAventureProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
