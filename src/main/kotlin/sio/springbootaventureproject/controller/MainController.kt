2f2d6ad040a13a322d85379727e032b2118fdf05
TRAN Nicolas <nicolas7725@gmail.com>
2023-11-15 11:55:15 +0100

    Séance 15/11

--- src/main/kotlin/sio/springbootaventureproject/controller/MainController.kt
+++ src/main/kotlin/sio/springbootaventureproject/controller/MainController.kt
@@ -1,4 +1,22 @@
 package sio.springbootaventureproject.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
 class MainController {
+    @GetMapping("/home")
+    fun index():String {
+        return "visiteur/home"
+    }
+
+    @GetMapping("/login")
+    fun login():String{
+        return "visiteur/login"
+    }
+
+    @GetMapping("/inscription")
+    fun inscription():String{
+        return "visiteur/inscription"
+    }
 }
--- src/main/kotlin/sio/springbootaventureproject/controller/admin/QualiteController.kt
+++ src/main/kotlin/sio/springbootaventureproject/controller/admin/QualiteController.kt
@@ -1,11 +1,7 @@
 package sio.springbootaventureproject.controller.admin
 
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.bind.annotation.GetMapping
-@RestController
+
+
 class QualiteController {
-    @GetMapping("/")
-    fun index():String {
-        return "accueil"
-    }
+
 }
