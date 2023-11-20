b885ddb7171b079d6ed2e186f881841716478e53
TRAN Nicolas <nicolas7725@gmail.com>
2023-11-20 11:53:29 +0100

    Fin TD

---
+++ src/main/kotlin/sio/springbootaventureproject/controller/model/dao/QualiteDao.kt
@@ -0,0 +1,7 @@
+package sio.springbootaventureproject.controller.model.dao
+
+import org.springframework.data.jpa.repository.JpaRepository
+import sio.springbootaventureproject.controller.model.entity.Qualite
+
+interface QualiteDao:JpaRepository<Qualite,Long> {
+}
--- src/main/kotlin/sio/springbootaventureproject/controller/model/entity/Qualite.kt
+++ src/main/kotlin/sio/springbootaventureproject/controller/model/entity/Qualite.kt
@@ -1,4 +1,15 @@
 package sio.springbootaventureproject.controller.model.entity
+import jakarta.persistence.*
+@Entity
+class Qualite constructor(
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY )
+    @Column(name = "id", nullable = false)
+    var id: Long? = null,
+    var nom: String,
+    var couleur: String,
+    var bonusQualite: Int
+    //Todo Mission 3 ajoutez les associations
+    ){
 
-class Qualite {
 }
